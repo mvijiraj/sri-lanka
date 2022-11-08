6a924a9335e052e476a3b482a9e8743cdb69b412
Vijay <mvijiraj@gmail.com>
2022-11-08 19:53:26 +0530

    the first commit

---
+++ index2.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+
+        <title>sms2 application</title>
+    </head>
+    <body>
+        SMS application 2
+        <button type="button" class="btn btn-primary">Primary</button>
+    </body>
+</html>
---
+++ templates2.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+
+        <title>sms2 application</title>
+        <link href='css/bootstrap.css' rel = "stylesheet"/>
+
+        <style>
+#topdiv{    
+    background-color:EA047E;
+    padding-top:25px;
+    color:white;
+}
+
+        </style>
+    </head>
+    <body>
+       
+        <div class="container">
+            <div class="row" id="topdiv">
+                <div class="col-sm-12">
+                    <h2>Student Management System</h2>
+                    <p>Your Digital School</p>
+
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    1
+                </div>
+                 <div class="col-sm-4">
+                    <img src="image/web.png" style="width:150px; margin-top:50px"/>
+                    <h3>Login</h3>
+                    <form action="">
+                    <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nic</label>
+                    <input type="text" name ="nic" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Password</label>
+                    <input type="Password" name ="pword" class="form-control">
+                </div>
+                <button type="submit" name ="btnsubmit"class="btn btn-primary">Login</button>
+
+                    </form>                    
+                </div>
+                <div class="col-sm-4">
+                    3
+                </div>
+            </div>
+            
+        </div>
+    </body>
+</html>
